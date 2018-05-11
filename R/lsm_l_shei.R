c18f7722ef1c610f6f846c7767d1d9568a0cb109
laurajanegraham <laurajaneegraham@gmail.com>
2018-05-11 16:19:56 +0100

    include calculation for shannon evenness/diversity

--- R/lsm_l_shei.R
+++ R/lsm_l_shei.R
@@ -1,15 +1,15 @@
-#' Shannon's Diversity Index
+#' Shannon's Evenness Index
 #'
-#' @description Diversity of patch types (classes) in the landscape
+#' @description Evenness of patch types (classes) in the landscape
 #'
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
 #' @return tibble
 #'
 #' @examples
-#' lsm_l_shdi(landscape)
+#' lsm_l_shei(landscape)
 #'
-#' @aliases lsm_l_shdi
-#' @rdname lsm_l_shdi
+#' @aliases lsm_l_shei
+#' @rdname lsm_l_shei
 #'
 #' @references
 #' McGarigal, K., and B. J. Marks. 1995. FRAGSTATS: spatial pattern analysis
@@ -17,7 +17,7 @@
 #'  PNW-351.
 #' @export
 
-lsm_l_shdi <- function(landscape) {
+lsm_l_shei <- function(landscape) {
 
     area <- raster::ncell(landscape)
 
@@ -26,17 +26,17 @@ lsm_l_shdi <- function(landscape) {
             raster::values() %>%
             table() / area
 
-        H <- tibble::tibble(
+        E <- tibble::tibble(
             layer = as.numeric(1),
             level = 'landscape',
             id = as.numeric(NA),
             metric = 'Shannon evenness',
-            value = sum(-p * log(p, exp(1)), na.rm = TRUE)
+            value = sum(-p * log(p, exp(1)), na.rm = TRUE) / log(length(p), exp(1))
         )
     }
 
     else {
-        H <- purrr::map_dfr(1:raster::nlayers(landscape), function(x){
+        E <- purrr::map_dfr(1:raster::nlayers(landscape), function(x){
             p <- landscape[[x]] %>%
                 raster::values() %>%
                 table() / area
@@ -45,14 +45,14 @@ lsm_l_shdi <- function(landscape) {
                 level = 'landscape',
                 id = as.numeric(NA),
                 metric = 'Shannon evenness',
-                value = sum(-p * log(p, exp(1)), na.rm = TRUE)
+                value = sum(-p * log(p, exp(1)), na.rm = TRUE) / log(length(p), exp(1))
             )
         }, .id = 'layer') %>%
             dplyr::mutate(layer = as.numeric(layer))
     }
 
 
-    return(H)
+    return(E)
 
 }
 
