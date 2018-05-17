0609f0139736f676696aead90bff832cfc04046a
Nowosad <tupiszakaczki@gmail.com>
2018-05-17 14:40:22 -0400

    data documentation updated

--- R/data.R
+++ R/data.R
@@ -1,18 +1,36 @@
 #' Example map (random cluster neutral landscape model).
 #'
 #' An example map to show landscapetools functionality
-#' generated with the nlm_randomcluster() algorithm.
+#' generated with the `nlm_randomcluster()` algorithm.
 #'
 #' @format A raster layer object.
-#' @source Simulated neutral landscape model with R. \url{https://github.com/ropensci/NLMR/}
+#' @source Simulated neutral landscape model with R. https://github.com/ropensci/NLMR/
 "landscape"
 
-
-#' Example maps as raster stack  (random cluster neutral landscape model).
+#' Example maps as raster stack (random cluster neutral landscape model).
 #'
-#' An example map rasterstack to show landscapetools functionality
-#' generated with the nlm_randomcluster() algorithm.
+#' An example rasterstack to show landscapetools functionality
+#' generated with `nlm_mpd()` and `nlm_random()`.
 #'
 #' @format A raster stack object.
-#' @source Simulated neutral landscape model with R. \url{https://github.com/ropensci/NLMR/}
+#' @source Simulated neutral landscape model with R. https://github.com/ropensci/NLMR/
 "landscape_stack"
+
+#' Augusta NLCD 2011
+#'
+#' A real landscape of area near Augusta, Georgia obtained from the National
+#' Land Cover Database (NLCD)
+#'
+#' @format A raster layer object.
+#' @source https://www.mrlc.gov/nlcd2011.php
+#' @references Homer, C.G., Dewitz, J.A., Yang, L., Jin, S., Danielson, P., Xian, G., Coulston, J., Herold, N.D., Wickham, J.D., and Megown, K., 2015, Completion of the 2011 National Land Cover Database for the conterminous United States-Representing a decade of land cover change information. Photogrammetric Engineering and Remote Sensing, v. 81, no. 5, p. 345-354
+"augusta_nlcd"
+
+#' Podlasie ESA CCI LC
+#'
+#' A real landscape of the Podlasie region in Poland from the ESA CCI Land
+#' Cover
+#'
+#' @format A raster layer object.
+#' @source http://maps.elie.ucl.ac.be/CCI/viewer/
+"podlasie_ccilc"
