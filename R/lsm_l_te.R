39df24e1b595a1bd08e7c7ea041464ac4b6ccf9b
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-06-13 14:35:19 +0200

    Bugfix total edge landscape level

--- R/lsm_l_te.R
+++ R/lsm_l_te.R
@@ -3,7 +3,8 @@
 #' @description  Total Edge
 #'
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
-#'
+#' @param count_boundary Include landscape boundary in edge length
+
 #' @return tibble
 #'
 #' @examples
@@ -18,12 +19,13 @@
 #'  PNW-351.
 #'
 #' @export
-lsm_l_te <- function(landscape) UseMethod("lsm_l_te")
+lsm_l_te <- function(landscape, count_boundary) UseMethod("lsm_l_te")
 
 #' @name lsm_l_te
 #' @export
-lsm_l_te.RasterLayer <- function(landscape) {
+lsm_l_te.RasterLayer <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_l_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
@@ -31,18 +33,9 @@ lsm_l_te.RasterLayer <- function(landscape) {
 
 #' @name lsm_l_te
 #' @export
-lsm_l_te.RasterStack <- function(landscape) {
+lsm_l_te.RasterStack <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
-                   .f = lsm_l_te_calc,
-                   .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-#' @name lsm_l_te
-#' @export
-lsm_l_te.RasterBrick <- function(landscape) {
-    purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_l_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
@@ -51,24 +44,51 @@ lsm_l_te.RasterBrick <- function(landscape) {
 
 #' @name lsm_l_te
 #' @export
-lsm_l_te.list <- function(landscape) {
+lsm_l_te.RasterBrick <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_l_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 
 }
 
-lsm_l_te_calc <- function(landscape){
+#' @name lsm_l_te
+#' @export
+lsm_l_te.list <- function(landscape, count_boundary = FALSE) {
+    purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
+                   .f = lsm_l_te_calc,
+                   .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
 
-    total_edge <- lsm_c_te_calc(landscape)
+}
+
+lsm_l_te_calc <- function(landscape, count_boundary){
+
+    if(isTRUE(count_boundary)){
+        landscape <- padding(landscape = landscape,
+                             padding_value = max(raster::values(landscape)) + 1,
+                             padding_cells = 1)
+    }
+
+
+
+    adjacent_cells <- raster::adjacent(landscape,
+                                       seq_len(raster::ncell(landscape)),
+                                       4,
+                                       pairs=TRUE)
+    # count whos neighbor of who
+    tb <- table(landscape[adjacent_cells[,1]],
+                landscape[adjacent_cells[,2]])
+    te <- sum(tb[lower.tri(tb)]) * raster::res(landscape)[[1]]
 
     tibble::tibble(
         level = "landscape",
         class = as.integer(NA),
         id = as.integer(NA),
         metric = "total edge",
-        value = sum(total_edge$value)
+        value = te
     )
 
 }
