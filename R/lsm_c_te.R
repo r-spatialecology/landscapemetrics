28df3313151401fb5eef2966b4e80634033f6119
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-06-13 14:07:18 +0200

    Fixed bug in lsm_c_te

--- R/lsm_c_te.R
+++ R/lsm_c_te.R
@@ -3,6 +3,7 @@
 #' @description Total edge of class (class level)
 #'
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
+#' @param count_boundary Include landscape boundary in edge length
 #'
 #' @details
 #' Total edge equals the sum of the length of all edges of class i. Total edge is an
@@ -27,12 +28,13 @@
 #'  PNW-351.
 #'
 #' @export
-lsm_c_te <- function(landscape) UseMethod("lsm_c_te")
+lsm_c_te <- function(landscape, count_boundary) UseMethod("lsm_c_te")
 
 #' @name lsm_c_te
 #' @export
-lsm_c_te.RasterLayer <- function(landscape) {
+lsm_c_te.RasterLayer <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_c_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
@@ -40,18 +42,9 @@ lsm_c_te.RasterLayer <- function(landscape) {
 
 #' @name lsm_c_te
 #' @export
-lsm_c_te.RasterStack <- function(landscape) {
+lsm_c_te.RasterStack <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
-                   .f = lsm_c_te_calc,
-                   .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-#' @name lsm_c_te
-#' @export
-lsm_c_te.RasterBrick <- function(landscape) {
-    purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_c_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
@@ -60,15 +53,27 @@ lsm_c_te.RasterBrick <- function(landscape) {
 
 #' @name lsm_c_te
 #' @export
-lsm_c_te.list <- function(landscape) {
+lsm_c_te.RasterBrick <- function(landscape, count_boundary = FALSE) {
     purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
                    .f = lsm_c_te_calc,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 
 }
 
-lsm_c_te_calc <- function(landscape) {
+#' @name lsm_c_te
+#' @export
+lsm_c_te.list <- function(landscape, count_boundary = FALSE) {
+    purrr::map_dfr(raster::as.list(landscape),
+                   count_boundary = count_boundary,
+                   .f = lsm_c_te_calc,
+                   .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+lsm_c_te_calc <- function(landscape, count_boundary) {
     # cclabel class
     cclabeled_raster <- cclabel(landscape)
 
@@ -78,6 +83,12 @@ lsm_c_te_calc <- function(landscape) {
                    function(x){
                        cclabeled_raster[[x]][is.na(cclabeled_raster[[x]])] <- -999
 
+                       if(isTRUE(count_boundary)){
+                       cclabeled_raster[[x]] <- padding(landscape = cclabeled_raster[[x]],
+                                                        padding_value = -999,
+                                                        padding_cells = 1)
+                       }
+
                        # compute neighborhood matrix
                        adjacent_cells <- raster::adjacent(cclabeled_raster[[x]],
                                                           seq_len(raster::ncell(cclabeled_raster[[x]])),
@@ -86,7 +97,7 @@ lsm_c_te_calc <- function(landscape) {
                        # count whos neighbor of who
                        tb <- table(cclabeled_raster[[x]][adjacent_cells[,1]],
                                    cclabeled_raster[[x]][adjacent_cells[,2]])
-                       te <- (sum(tb[2:ncol(tb),1])/2) * prod(raster::res(landscape))
+                       te <- (sum(tb[2:ncol(tb),1])) * raster::res(landscape)[[1]]
                    })
 
     # return first row with counts of adjents sites between patches and
