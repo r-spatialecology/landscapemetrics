cca41ceb5a39413de713d1b50385cbb4df094aa6
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-06-18 08:22:58 +0200

    Missing ENN distribution landscape level

---
+++ R/lsm_l_enn_sd.R
@@ -0,0 +1,75 @@
+#' Euclidean Nearest Neighbor Distance Distribution (landscape level)
+#'
+#' @description Standard deviation Euclidean Nearest Neighbor Distance (landscape level)
+#'
+#' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
+#'
+#' @details
+#' Equals the standard deviation of the eclidean nearest neighbor distance of all patches
+#' in the landscape. ENN equals the distance the the nearest neigbouring patch of the same
+#' patch type (shortest edge-to-edge distance)
+#' \deqn{ENN_MN = mean(ENN[patch])}
+#' \subsection{Units}{Meters}
+#' \subsection{Range}{???}
+#' \subsection{Behaviour}{???}
+#'
+#' @return tibble
+#'
+#' @examples
+#' lsm_l_enn_sd(landscape)
+#'
+#' @aliases lsm_l_enn_sd
+#' @rdname lsm_l_enn_sd
+#'
+#' @references
+#' McGarigal, K., and B. J. Marks. 1995. FRAGSTATS: spatial pattern analysis
+#' program for quantifying landscape structure. USDA For. Serv. Gen. Tech. Rep.
+#'  PNW-351.
+#'
+#' @export
+lsm_l_enn_sd <- function(landscape) UseMethod("lsm_l_enn_sd")
+
+#' @name lsm_l_enn_sd
+#' @export
+lsm_l_enn_sd.RasterLayer <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_l_enn_sd_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+}
+
+#' @name lsm_l_enn_sd
+#' @export
+lsm_l_enn_sd.RasterStack <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_l_enn_sd_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+#' @name lsm_l_enn_sd
+#' @export
+lsm_l_enn_sd.RasterBrick <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_l_enn_sd_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+#' @name lsm_l_enn_sd
+#' @export
+lsm_l_enn_sd.list <- function(landscape) {
+    purrr::map_dfr(landscape, lsm_l_enn_sd_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+}
+
+lsm_l_enn_sd_calc <- function(landscape) {
+
+    enn_sd  <- lsm_p_enn_calc(landscape) %>%
+        dplyr::summarize(value = stats::sd(value, na.rm = TRUE))
+
+    tibble::tibble(
+        level = "landscape",
+        class = as.integer(NA),
+        id = as.integer(NA),
+        metric = "euclidean nearest neighbor distance distribution (sd)",
+        value = enn_sd$value
+    )
+
+}
