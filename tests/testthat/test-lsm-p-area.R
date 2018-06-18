aecd12df1cf4e763b57a306bdd5015962edafc54
marcosci <sciaini.marco@gmail.com>
2018-06-18 18:38:53 +0200

    update test structure

--- tests/testthat/test-lsm-p-area.R
+++ tests/testthat/test-lsm-p-area.R
@@ -1,8 +1,31 @@
 context("patch level area metric")
 
-fragstats_values <- fragstats_patch_landscape$AREA
-landscapemetrics_values <- lsm_p_area(landscape)$value
+fragstats_patch_landscape_area <- fragstats_patch_landscape$AREA
+fragstats_patch_landscapestack_area <- fragstats_patch_landscapestack$AREA
+fragstats_patch_augusta_nlcd_area <- fragstats_patch_augusta_nlcd$AREA
+fragstats_patch_podlasie_area <- fragstats_patch_podlasie$AREA
+landscapemetrics_patch_landscape_area <- lsm_p_area(landscape)
+landscapemetrics_patch_landscape_stack_area <- lsm_p_area(landscape_stack)
+landscapemetrics_patch_augusta_nlcd_area <- lsm_p_area(augusta_nlcd)
+landscapemetrics_patch_podlasie_ccilc_area <- lsm_p_area(podlasie_ccilc)
 
-test_that("landscape level results are equal to fragstats", {
-        expect_true(all(fragstats_values %in% landscapemetrics_values))
+test_that("lsm_p_area results are equal to fragstats", {
+    expect_true(all(fragstats_patch_landscape_area %in%
+                        landscapemetrics_patch_landscape_area$value))
+    expect_true(all(fragstats_patch_landscapestack_area %in%
+                        landscapemetrics_patch_landscape_stack_area$value))
+    expect_true(all(fragstats_patch_augusta_nlcd_area %in%
+                        landscapemetrics_patch_augusta_nlcd_area$value))
+    expect_true(all(fragstats_patch_podlasie_area %in%
+                        landscapemetrics_patch_podlasie_ccilc_area$value))
+})
+
+test_that("lsm_p_area is typestable", {
+    expect_is(lsm_p_area(landscape), "tbl_df")
+    expect_is(lsm_p_area(landscape_stack), "tbl_df")
+    expect_is(lsm_p_area(list(landscape, landscape)), "tbl_df")
+})
+
+test_that("lsm_p_area returns the desired number of columns", {
+    expect_equal(ncol(landscapemetrics_values), 6)
 })
