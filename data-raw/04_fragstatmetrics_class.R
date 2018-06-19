e3680b3934887fc0e970e981ca9812244dc2a717
marcosci <sciaini.marco@gmail.com>
2018-06-19 17:50:40 +0200

    implemented scripts to include class and landscapelevel results from fragstats

---
+++ data-raw/04_fragstatmetrics_class.R
@@ -0,0 +1,27 @@
+library(readr)
+library(tidyverse)
+
+# load fragstats results for class level
+fragstats_class <- read_csv("data-raw/fragstats_results_class.txt")
+
+# filter for augusta_nlcd raster
+fragstats_class_augusta_nlcd <- fragstats_class %>%
+    filter(str_detect(LID, 'augusta_nlcd'))
+
+# filter for landscape raster
+fragstats_class_landscape <- fragstats_class %>%
+    filter(str_detect(LID, 'landscape.tif'))
+
+# filter for landscape raster stack
+fragstats_class_landscapestack <- fragstats_class %>%
+    filter(str_detect(LID, 'landscape_stack'))
+
+# filter for podlasie raster
+fragstats_class_podlasie <- fragstats_class %>%
+    filter(str_detect(LID, 'podlasie'))
+
+# save --------------------------------------------------------------------
+devtools::use_data(fragstats_class_augusta_nlcd, overwrite = TRUE)
+devtools::use_data(fragstats_class_landscape, overwrite = TRUE)
+devtools::use_data(fragstats_class_landscapestack, overwrite = TRUE)
+devtools::use_data(fragstats_class_podlasie, overwrite = TRUE)
---
+++ data-raw/05_fragstatmetrics_landscape.R
@@ -0,0 +1,27 @@
+library(readr)
+library(tidyverse)
+
+# load fragstats results for landscape level
+fragstats_landscape <- read_csv("data-raw/fragstats_results_land.txt")
+
+# filter for augusta_nlcd raster
+fragstats_landscape_augusta_nlcd <- fragstats_landscape %>%
+    filter(str_detect(LID, 'augusta_nlcd'))
+
+# filter for landscape raster
+fragstats_landscape_landscape <- fragstats_landscape %>%
+    filter(str_detect(LID, 'landscape.tif'))
+
+# filter for landscape raster stack
+fragstats_landscape_landscapestack <- fragstats_landscape %>%
+    filter(str_detect(LID, 'landscape_stack'))
+
+# filter for podlasie raster
+fragstats_landscape_podlasie <- fragstats_landscape %>%
+    filter(str_detect(LID, 'podlasie'))
+
+# save --------------------------------------------------------------------
+devtools::use_data(fragstats_landscape_augusta_nlcd, overwrite = TRUE)
+devtools::use_data(fragstats_landscape_landscape, overwrite = TRUE)
+devtools::use_data(fragstats_landscape_landscapestack, overwrite = TRUE)
+devtools::use_data(fragstats_landscape_podlasie, overwrite = TRUE)
