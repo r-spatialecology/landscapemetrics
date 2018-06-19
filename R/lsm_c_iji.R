42cb0303720112f22a0a52ed04e7cb98d991649e
mhesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-06-19 17:22:37 +0200

    Removed lsm_c_iji() (not working at all currently)

--- R/lsm_c_iji.R
+++ R/lsm_c_iji.R
@@ -1,96 +1,96 @@
-#' Interspersion and Juxtaposition index (class level)
+#' #' Interspersion and Juxtaposition index (class level)
+#' #'
+#' #' @description Interspersion and Juxtaposition index (class level)
+#' #'
+#' #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
+#' #' @param count_boundary ???
+#' #'
+#' #' @details
+#' #' ???
+#' #' \deqn{???}
+#' #' \subsection{Units}{???}
+#' #' \subsection{Range}{???}
+#' #' \subsection{Behaviour}{???}
+#' #'
+#' #' @return tibble
+#' #'
+#' #' @examples
+#' #' lsm_c_iji(landscape)
+#' #'
+#' #' @aliases lsm_c_iji
+#' #' @rdname lsm_c_iji
+#' #'
+#' #' @references
+#' #' McGarigal, K., and B. J. Marks. 1995. FRAGSTATS: spatial pattern analysis
+#' #' program for quantifying landscape structure. USDA For. Serv. Gen. Tech. Rep.
+#' #'  PNW-351.
+#' #'
+#' #' @export
+#' lsm_c_iji <- function(landscape, count_boundary) UseMethod("lsm_c_iji")
 #'
-#' @description Interspersion and Juxtaposition index (class level)
+#' #' @name lsm_c_iji
+#' #' @export
+#' lsm_c_iji.RasterLayer <- function(landscape, count_boundary = FALSE) {
+#'     purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
+#'                    count_boundary = count_boundary, .id = "layer") %>%
+#'         dplyr::mutate(layer = as.integer(layer))
+#' }
 #'
-#' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
-#' @param count_boundary ???
+#' #' @name lsm_c_iji
+#' #' @export
+#' lsm_c_iji.RasterStack <- function(landscape, count_boundary = FALSE) {
+#'     purrr::map_dfr(raster::as.list(landscape),
+#'                    lsm_c_iji_calc,
+#'                    count_boundary = count_boundary, .id = "layer") %>%
+#'         dplyr::mutate(layer = as.integer(layer))
 #'
-#' @details
-#' ???
-#' \deqn{???}
-#' \subsection{Units}{???}
-#' \subsection{Range}{???}
-#' \subsection{Behaviour}{???}
+#' }
 #'
-#' @return tibble
+#' #' @name lsm_c_iji
+#' #' @export
+#' lsm_c_iji.RasterBrick <- function(landscape, count_boundary = FALSE) {
+#'     purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
+#'                    count_boundary = count_boundary, .id = "layer") %>%
+#'         dplyr::mutate(layer = as.integer(layer))
 #'
-#' @examples
-#' lsm_c_iji(landscape)
+#' }
 #'
-#' @aliases lsm_c_iji
-#' @rdname lsm_c_iji
+#' #' @name lsm_c_iji
+#' #' @export
+#' lsm_c_iji.list <- function(landscape, count_boundary = FALSE) {
+#'     purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
+#'                    count_boundary = count_boundary, .id = "layer") %>%
+#'         dplyr::mutate(layer = as.integer(layer))
 #'
-#' @references
-#' McGarigal, K., and B. J. Marks. 1995. FRAGSTATS: spatial pattern analysis
-#' program for quantifying landscape structure. USDA For. Serv. Gen. Tech. Rep.
-#'  PNW-351.
+#' }
 #'
-#' @export
-lsm_c_iji <- function(landscape, count_boundary) UseMethod("lsm_c_iji")
-
-#' @name lsm_c_iji
-#' @export
-lsm_c_iji.RasterLayer <- function(landscape, count_boundary = FALSE) {
-    purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
-                   count_boundary = count_boundary, .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-}
-
-#' @name lsm_c_iji
-#' @export
-lsm_c_iji.RasterStack <- function(landscape, count_boundary = FALSE) {
-    purrr::map_dfr(raster::as.list(landscape),
-                   lsm_c_iji_calc,
-                   count_boundary = count_boundary, .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-#' @name lsm_c_iji
-#' @export
-lsm_c_iji.RasterBrick <- function(landscape, count_boundary = FALSE) {
-    purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
-                   count_boundary = count_boundary, .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-#' @name lsm_c_iji
-#' @export
-lsm_c_iji.list <- function(landscape, count_boundary = FALSE) {
-    purrr::map_dfr(raster::as.list(landscape), lsm_c_iji_calc,
-                   count_boundary = count_boundary, .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-# Not working currently
-lsm_c_iji_calc <- function(landscape, count_boundary = FALSE) {
-
-    edge_class <- lsm_c_te_calc(landscape, count_boundary = count_boundary)
-    pr <- lsm_l_pr_calc(landscape)
-
-    iji <- landscape %>%
-        lsm_p_perim_calc() %>%
-        dplyr::mutate(value = (value / edge_class$value[class]) *
-                          log((value / edge_class$value[class]))) %>%
-        dplyr::group_by(class) %>%
-        dplyr::summarise(value = -sum(value, na.rm = TRUE)) %>%
-        dplyr::mutate(value = (value / log(pr$value - 1)) * 100)
-
-    tibble::tibble(
-        level = "class",
-        class = iji$class,
-        id = as.integer(NA),
-        metric = "interspersion and juxtaposition index",
-        value = iji$value
-    )
-}
-
-
-
-
-
-
-
-
+#' # Not working currently
+#' lsm_c_iji_calc <- function(landscape, count_boundary = FALSE) {
+#'
+#'     edge_class <- lsm_c_te_calc(landscape, count_boundary = count_boundary)
+#'     pr <- lsm_l_pr_calc(landscape)
+#'
+#'     iji <- landscape %>%
+#'         lsm_p_perim_calc() %>%
+#'         dplyr::mutate(value = (value / edge_class$value[class]) *
+#'                           log((value / edge_class$value[class]))) %>%
+#'         dplyr::group_by(class) %>%
+#'         dplyr::summarise(value = -sum(value, na.rm = TRUE)) %>%
+#'         dplyr::mutate(value = (value / log(pr$value - 1)) * 100)
+#'
+#'     tibble::tibble(
+#'         level = "class",
+#'         class = iji$class,
+#'         id = as.integer(NA),
+#'         metric = "interspersion and juxtaposition index",
+#'         value = iji$value
+#'     )
+#' }
+#'
+#'
+#'
+#'
+#'
+#'
+#'
+#'
