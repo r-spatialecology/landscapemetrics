d8ebe88f6a6c3cae20935a483085276f440ad21a
jn <tupiszakaczki@gmail.com>
2018-07-02 20:30:43 -0400

    landscape complexity improved

--- R/lsm_l_cplx.R
+++ R/lsm_l_cplx.R
@@ -15,15 +15,7 @@
 #' @return tibble
 #'
 #' @examples
-#' library(raster)
-#' test1 = raster(nrows = 3, ncols = 3, vals = c(rep(1, 8), 2))
-#' test2 = as.matrix(test1)
-#' test3 = raster(nrows = 3, ncols = 3, vals = c(3, rep(1, 7), NA))
-#'
-#' lsm_l_cplx(test1)
-#' lsm_l_cplx(test1, directions = 4)
-#' lsm_l_cplx(test2)
-#' lsm_l_cplx(test3)
+#' lsm_l_cplx(landscape)
 #'
 #' @aliases lsm_l_cplx
 #' @rdname lsm_l_cplx
@@ -33,19 +25,29 @@
 #' landscape complexity. DOI:
 #'
 #' @export
-lsm_l_cplx <- function(landscape, directions = 4, ordered = TRUE, base = "log2") UseMethod("lsm_l_cplx")
+lsm_l_cplx <- function(landscape, directions, ordered, base) UseMethod("lsm_l_cplx")
 
 #' @name lsm_l_cplx
 #' @export
 lsm_l_cplx.RasterLayer <- function(landscape, directions = 4, ordered = TRUE, base = "log2") {
-    purrr::map_dfr(raster::as.list(landscape), lsm_l_cplx_calc, .id = "layer") %>%
+    purrr::map_dfr(raster::as.list(landscape),
+                   lsm_l_cplx_calc,
+                   directions = directions,
+                   ordered = ordered,
+                   base = base,
+                   .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 }
 
 #' @name lsm_l_cplx
 #' @export
 lsm_l_cplx.RasterStack <- function(landscape, directions = 4, ordered = TRUE, base = "log2") {
-    purrr::map_dfr(raster::as.list(landscape), lsm_l_cplx_calc, .id = "layer") %>%
+    purrr::map_dfr(raster::as.list(landscape),
+                   lsm_l_cplx_calc,
+                   directions = directions,
+                   ordered = ordered,
+                   base = base,
+                   .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 
 }
@@ -53,7 +55,12 @@ lsm_l_cplx.RasterStack <- function(landscape, directions = 4, ordered = TRUE, ba
 #' @name lsm_l_cplx
 #' @export
 lsm_l_cplx.RasterBrick <- function(landscape, directions = 4, ordered = TRUE, base = "log2") {
-    purrr::map_dfr(raster::as.list(landscape), lsm_l_cplx_calc, .id = "layer") %>%
+    purrr::map_dfr(raster::as.list(landscape),
+                   lsm_l_cplx_calc,
+                   directions = directions,
+                   ordered = ordered,
+                   base = base,
+                   .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 
 }
@@ -61,14 +68,27 @@ lsm_l_cplx.RasterBrick <- function(landscape, directions = 4, ordered = TRUE, ba
 #' @name lsm_l_cplx
 #' @export
 lsm_l_cplx.list <- function(landscape, directions = 4, ordered = TRUE, base = "log2") {
-    purrr::map_dfr(landscape, lsm_l_cplx_calc, .id = "layer") %>%
+    purrr::map_dfr(landscape,
+                   lsm_l_cplx_calc,
+                   directions = directions,
+                   ordered = ordered,
+                   base = base,
+                   .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
-
-
 }
 
-lsm_l_cplx_calc <- function(landscape, directions = 4, ordered = TRUE, base = "log2"){
+lsm_l_cplx_calc <- function(landscape, directions, ordered, base){
     landscape_matrix <- raster::as.matrix(landscape)
-    coh <- rcpp_get_coocurrence_vector(landscape_matrix, directions, ordered)
-    rcpp_get_entropy(coh, base)
+    coh <- rcpp_get_coocurrence_vector(landscape_matrix,
+                                       directions = directions,
+                                       ordered = ordered)
+    cplx <- rcpp_get_entropy(coh, base)
+
+    tibble::tibble(
+        level = "landscape",
+        class = as.integer(NA),
+        id = as.integer(NA),
+        metric = "complexity",
+        value = as.double(cplx)
+    )
 }
