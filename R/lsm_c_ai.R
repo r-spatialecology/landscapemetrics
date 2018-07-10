d63edfeceb2ebcfd7aeb664702bbe831f6bb4825
marcosci <sciaini.marco@gmail.com>
2018-07-10 19:36:16 +0200

    AI implemented ... something I always wanted to say

---
+++ R/lsm_c_ai.R
@@ -0,0 +1,110 @@
+#' AI (class level)
+#'
+#' @description Aggregation index (Contagion/Interspersion metric)
+#'
+#' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers
+#'
+#' @details
+#' \deqn{AI = \Bigg[\frac{g_{ii}}{max-g_{ii}} \Bigg](100) }
+#'
+#' where \eqn{g_{ii}} is the number of like adjacencies based on the single-count method and
+#' \eqn{max-g_{ii}} is the classwise maximum number of like adjacencies of class i.
+#'
+#' CLUMPY is an 'Contagion/Interspersion metric'. It equals the number of like adjacencies divided
+#' by the theoretical maximum possible number of like adjacencies for that class.
+#' The metric is based on he adjacency matrix and the the single-count method.
+#'
+#' \subsection{Units}{Percent}
+#' \subsection{Range}{0 <= AI <= 100}
+#' \subsection{Behaviour}{Equals 0 for maximally disaggregated and 100
+#'  for maximally aggregated classes.}
+#'
+#' @return tibble
+#'
+#' @examples
+#' lsm_c_ai(landscape)
+#'
+#' @aliases lsm_c_ai
+#' @rdname lsm_c_ai
+#'
+#' @references
+#' McGarigal, K., SA Cushman, and E Ene. 2012. FRAGSTATS v4: Spatial Pattern Analysis
+#' Program for Categorical and Continuous Maps. Computer software program produced by
+#' the authors at the University of Massachusetts, Amherst. Available at the following
+#' web site: http://www.umass.edu/landeco/research/fragstats/fragstats.html
+#'
+#' @export
+lsm_c_ai <- function(landscape) UseMethod("lsm_c_ai")
+
+#' @name lsm_c_ai
+#' @export
+lsm_c_ai.RasterLayer <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_c_ai_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+}
+
+#' @name lsm_c_ai
+#' @export
+lsm_c_ai.RasterStack <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_c_ai_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+#' @name lsm_c_ai
+#' @export
+lsm_c_ai.RasterBrick <- function(landscape) {
+    purrr::map_dfr(raster::as.list(landscape), lsm_c_ai_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+#' @name lsm_c_ai
+#' @export
+lsm_c_ai.list <- function(landscape) {
+    purrr::map_dfr(landscape, lsm_c_ai_calc, .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+
+lsm_c_ai_calc <- function(landscape) {
+    adjacent_cells <- raster::adjacent(
+        landscape,
+        cells =  seq_len(raster::ncell(landscape)),
+        directions = 4,
+        pairs = TRUE
+    )
+
+    tb <- table(landscape[adjacent_cells[, 1]],
+                landscape[adjacent_cells[, 2]])
+
+    like_adjacencies <- diag(tb) / 2
+
+    area_class <-
+        tibble::as.tibble(raster::freq(landscape, useNA = "no"))
+
+    min_e <- dplyr::mutate(
+        area_class,
+        value = count * 10000,
+        n = trunc(sqrt(count)),
+        m = count - n ^ 2,
+        min_e = dplyr::case_when(
+            m == 0 ~ 2 * n * (n - 1),
+            m <= n ~ 2 * n * (n - 1) + 2 * m - 1,
+            m > n ~ 2 * n * (n - 1) + 2 * m - 2
+        )
+    ) %>%
+        dplyr::pull(min_e)
+
+    ai <- (like_adjacencies / min_e) * 100
+
+
+    tibble::tibble(
+        level = "class",
+        class = as.integer(seq_along(ai)),
+        id = as.integer(NA),
+        metric = "ai",
+        value = as.double(ai)
+    )
+}
---
+++ tests/testthat/test-lsm-c-ai.R
@@ -0,0 +1,31 @@
+context("class level ai cv metric")
+
+# fragstats_class_landscape_ai <- fragstats_class_landscape$ai
+fragstats_class_landscape_ai <- fragstats_class_landscape$AI
+
+landscapemetrics_class_landscape_ai <- lsm_c_ai(landscape)
+
+test_that("lsm_c_ai results are equal to fragstats", {
+    expect_true(all(fragstats_class_landscape_ai %in%
+                        round(landscapemetrics_class_landscape_ai$value, 4)))
+})
+
+test_that("lsm_c_ai is typestable", {
+    expect_is(landscapemetrics_class_landscape_ai, "tbl_df")
+    expect_is(lsm_c_ai(landscape_stack), "tbl_df")
+    expect_is(lsm_c_ai(list(landscape, landscape)), "tbl_df")
+})
+
+test_that("lsm_c_ai returns the desired number of columns", {
+    expect_equal(ncol(landscapemetrics_class_landscape_ai), 6)
+})
+
+test_that("lsm_c_ai returns in every column the correct type", {
+    expect_type(landscapemetrics_class_landscape_ai$layer, "integer")
+    expect_type(landscapemetrics_class_landscape_ai$level, "character")
+    expect_type(landscapemetrics_class_landscape_ai$class, "integer")
+    expect_type(landscapemetrics_class_landscape_ai$id, "integer")
+    expect_type(landscapemetrics_class_landscape_ai$metric, "character")
+    expect_type(landscapemetrics_class_landscape_ai$value, "double")
+})
+
