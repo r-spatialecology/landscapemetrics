6d00aa3db0bf26c57c5dda9e602aff02f66fb048
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-07-18 15:24:08 +0200

    Rcpp code

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -141,12 +141,12 @@ ccl_labels <- function(m) {
     .Call('_landscapemetrics_ccl_labels', PACKAGE = 'landscapemetrics', m)
 }
 
-min_dist_fun <- function(point_a, point_b) {
-    .Call('_landscapemetrics_min_dist_fun', PACKAGE = 'landscapemetrics', point_a, point_b)
+rcpp_get_circle <- function(points, resolution) {
+    .Call('_landscapemetrics_rcpp_get_circle', PACKAGE = 'landscapemetrics', points, resolution)
 }
 
-max_dist_fun <- function(point_a, point_b) {
-    .Call('_landscapemetrics_max_dist_fun', PACKAGE = 'landscapemetrics', point_a, point_b)
+min_dist_fun <- function(point_a, point_b) {
+    .Call('_landscapemetrics_min_dist_fun', PACKAGE = 'landscapemetrics', point_a, point_b)
 }
 
 rcpp_get_nearest_neighbor <- function(points) {
