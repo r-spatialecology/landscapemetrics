c9d6aa9af114031b28d7c46a028119dda6dc761c
marco <sciaini.marco@gmail.com>
2018-07-24 15:16:02 +0200

    i am maybe to stupid for tics...

--- tic.R
+++ tic.R
@@ -9,7 +9,7 @@ if (Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy())
+  # get_stage("deploy") %>%
+  #   add_step(step_build_pkgdown()) %>%
+  #   add_step(step_push_deploy())
 }
