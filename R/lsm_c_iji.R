fc4702b33ae6e6eb944d3b548b6e29d766e53e30
marcosci <sciaini.marco@gmail.com>
2018-07-25 21:40:51 +0200

    implemented lsm_c_iji

--- R/lsm_c_iji.R
+++ R/lsm_c_iji.R
@@ -29,82 +29,65 @@
 #'web site: http://www.umass.edu/landeco/research/fragstats/fragstats.html
 #
 #' @export
-lsm_c_iji <- function(landscape,
-                      count_boundary, directions) UseMethod("lsm_c_iji")
+lsm_c_iji <- function(landscape) UseMethod("lsm_c_iji")
 #' @name lsm_c_iji
 #' @export
-lsm_c_iji.RasterLayer <- function(landscape,
-                                  count_boundary = FALSE, directions = 8) {
+lsm_c_iji.RasterLayer <- function(landscape) {
  purrr::map_dfr(raster::as.list(landscape),
                 lsm_c_iji_calc,
-                count_boundary = count_boundary,
-                directions = directions,
                 .id = "layer") %>%
      dplyr::mutate(layer = as.integer(layer))
 }
 #' @name lsm_c_iji
 #' @export
-lsm_c_iji.RasterStack <- function(landscape,
-                                  count_boundary = FALSE,
-                                  directions = 8) {
+lsm_c_iji.RasterStack <- function(landscape) {
  purrr::map_dfr(raster::as.list(landscape),
                 lsm_c_iji_calc,
-                count_boundary = count_boundary,
-                directions = directions,
                 .id = "layer") %>%
      dplyr::mutate(layer = as.integer(layer))
 }
 #' @name lsm_c_iji
 #' @export
-lsm_c_iji.RasterBrick <- function(landscape,
-                                  count_boundary = FALSE,
-                                  directions = 8) {
+lsm_c_iji.RasterBrick <- function(landscape) {
  purrr::map_dfr(raster::as.list(landscape),
                 lsm_c_iji_calc,
-                count_boundary = count_boundary,
-                directions = directions,
                 .id = "layer") %>%
      dplyr::mutate(layer = as.integer(layer))
 }
 #' @name lsm_c_iji
 #' @export
-lsm_c_iji.list <- function(landscape,
-                           count_boundary = FALSE, directions = 8) {
+lsm_c_iji.list <- function(landscape) {
  purrr::map_dfr(raster::as.list(landscape),
                 lsm_c_iji_calc,
-                count_boundary = count_boundary,
-                directions = directions,
                 .id = "layer") %>%
      dplyr::mutate(layer = as.integer(layer))
 }
 
-# Not working currently
-lsm_c_iji_calc <- function(landscape, count_boundary, directions) {
+lsm_c_iji_calc <- function(landscape) {
+
+    adj <- rcpp_get_coocurrence_matrix(raster::as.matrix(landscape),
+                                       as.matrix(4))
+
+    diag(adj) <- 0
+
+    iji <- purrr::map(seq_len(ncol(adj)), function(row_ind) {
+        purrr::map_dbl(seq_len(ncol(adj)), function(col_ind) {
+            (adj[row_ind, col_ind] / sum(adj[row_ind, ])) *
+                log((adj[row_ind, col_ind] / sum(adj[row_ind, ])))
+        })
+    })
 
 
- edges <- rcpp_get_coocurrence_matrix(raster::as.matrix(landscape),
-                                      directions = as.matrix(4))
- edges_class <- colSums(edges)
+    iji <- purrr::map_dbl(iji, sum, na.rm = TRUE)
 
- iji_step1 <- purrr::map(seq_along(edges_class), function(i){
+    iji <- -(iji / log(ncol(adj) - 1)) * 100
 
-     edges[,i] / edges_class[i]
-
- })
-
- iji_step2 <- purrr::map(iji_step1, log)
-
-
- iji_step3 <- purrr::map_dbl(seq_along(iji_step1), function(i){
-
-    - sum(iji_step1[[i]] * iji_step2[[i]])
- })
 
  tibble::tibble(
      level = "class",
-     class = as.integer(names(edges_class)),
+     class = as.integer(raster::unique(landscape)),
      id = as.integer(NA),
      metric = "iji",
-     value = (iji_step3 / log(length(edges_class - 1))) * 100
+     value = as.double(iji)
  )
 }
