2a29f0bb59ffc23b2ecf8b66161046aa4227b440
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-08-01 14:41:36 +0200

    Forgot verbose for lsm_l_enn_xx()

--- R/lsm_l_enn_sd.R
+++ R/lsm_l_enn_sd.R
@@ -4,6 +4,7 @@
 #'
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
 #' @param directions The number of directions in which patches should be connected: 4 (rook's case) or 8 (queen's case).
+#' @param verbose Print warning message if not sufficient patches are present
 #'
 #' @details
 #' \deqn{ENN_{SD} = sd(ENN[patch_{ij}])}
@@ -46,35 +47,26 @@
 #' web site: http://www.umass.edu/landeco/research/fragstats/fragstats.html
 #'
 #' @export
-lsm_l_enn_sd <- function(landscape, directions) UseMethod("lsm_l_enn_sd")
+lsm_l_enn_sd <- function(landscape, directions, verbose) UseMethod("lsm_l_enn_sd")
 
 #' @name lsm_l_enn_sd
 #' @export
-lsm_l_enn_sd.RasterLayer <- function(landscape, directions = 8) {
+lsm_l_enn_sd.RasterLayer <- function(landscape, directions = 8, verbose = TRUE) {
     purrr::map_dfr(raster::as.list(landscape),
                    lsm_l_enn_sd_calc,
                    directions = directions,
+                   verbose = verbose,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 }
 
 #' @name lsm_l_enn_sd
 #' @export
-lsm_l_enn_sd.RasterStack <- function(landscape, directions = 8) {
+lsm_l_enn_sd.RasterStack <- function(landscape, directions = 8, verbose = TRUE) {
     purrr::map_dfr(raster::as.list(landscape),
                    lsm_l_enn_sd_calc,
                    directions = directions,
-                   .id = "layer") %>%
-        dplyr::mutate(layer = as.integer(layer))
-
-}
-
-#' @name lsm_l_enn_sd
-#' @export
-lsm_l_enn_sd.RasterBrick <- function(landscape, directions = 8) {
-    purrr::map_dfr(raster::as.list(landscape),
-                   lsm_l_enn_sd_calc,
-                   directions = directions,
+                   verbose = verbose,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 
@@ -82,18 +74,32 @@ lsm_l_enn_sd.RasterBrick <- function(landscape, directions = 8) {
 
 #' @name lsm_l_enn_sd
 #' @export
-lsm_l_enn_sd.list <- function(landscape, directions = 8) {
+lsm_l_enn_sd.RasterBrick <- function(landscape, directions = 8, verbose = TRUE) {
+    purrr::map_dfr(raster::as.list(landscape),
+                   lsm_l_enn_sd_calc,
+                   directions = directions,
+                   verbose = verbose,
+                   .id = "layer") %>%
+        dplyr::mutate(layer = as.integer(layer))
+
+}
+
+#' @name lsm_l_enn_sd
+#' @export
+lsm_l_enn_sd.list <- function(landscape, directions = 8, verbose = TRUE) {
     purrr::map_dfr(landscape,
                    lsm_l_enn_sd_calc,
                    directions = directions,
+                   verbose = verbose,
                    .id = "layer") %>%
         dplyr::mutate(layer = as.integer(layer))
 }
 
-lsm_l_enn_sd_calc <- function(landscape, directions) {
+lsm_l_enn_sd_calc <- function(landscape, directions, verbose) {
 
     enn_sd  <- landscape %>%
-        lsm_p_enn_calc(., directions = directions) %>%
+        lsm_p_enn_calc(directions = directions,
+                       verbose = verbose) %>%
         dplyr::summarize(value = stats::sd(value))
 
     tibble::tibble(
