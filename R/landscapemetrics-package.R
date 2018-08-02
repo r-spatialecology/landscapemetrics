ba10cce8b9a982dc0f0d1a3649bf5915cf7f68e5
marco <sciaini.marco@gmail.com>
2018-08-02 09:10:48 +0200

    update description and package.r

--- R/landscapemetrics-package.R
+++ R/landscapemetrics-package.R
@@ -1,6 +1,14 @@
 #' @title landscapemetrics
 #'
-#' @description xxx
+#' @description
+#' Calculates landscape metrics for categorical landscape patterns in
+#' a tidy workflow. 'landscapemetrics' reimplements the most common metrics from
+#' FRAGSTATS and new ones from the current literature on landscape metrics.
+#' This package supports raster spatial objects and takes
+#' RasterLayer, RasterStacks, RasterBricks or lists of RasterLayer from the
+#' 'raster' package as input arguments. It further provides utility functions
+#' to visualize patches, select metrics and building blocks to develop new
+#' metrics.
 #'
 #' @name landscapemetrics
 #' @docType package
