8a7235c3b7c2784b2d268b53adfbeb7f2b32efa1
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-08-14 15:33:36 +0200

    Busy busy busy on sampling

--- R/sample_metrics.R
+++ R/sample_metrics.R
@@ -68,6 +68,36 @@ sample_metrics.list <- function(landscape,
 sample_metrics_int <- function(landscape, what,
                                points, shape, size) {
 
+    if (shape == "circle") {
+
+        circle_points <- seq(0, 2 * pi, length.out = 100)
+        circle_points_x <- sin(circle_points) * size
+        circle_points_y <- cos(circle_points) * size
+
+        x_circle <- matrix(outer(circle_points_x, points[, 1], `+`),
+                           ncol = 1)
+
+        y_circle <- matrix(outer(circle_points_y, points[, 2], `+`),
+                           ncol = 1)
+
+        sample_plots_coords <- cbind(x_circle, y_circle,
+                                     rep(1:nrow(points), each = 100))
+
+        # Polygons(sample_plots_coords[, 1:2], ID = sample_plots_coords[, 3])
+
+        # sample_plots <- SpatialPolygons(list(Polygons(list(Polygon(xy)), "Polygon")))
+
+
+    }
+
+    else if (shape == "square") {
+
+    }
+
+    else{
+        stop(paste0("Shape=", shape, " unknown"))
+    }
+
 
 
 }
@@ -76,11 +106,19 @@ sample_metrics_int <- function(landscape, what,
 # y = 10 # center y
 # n = 100 # nr of pts
 # r = 5 # radius
+#
 # pts = seq(0, 2 * pi, length.out = n)
 # plot(sin(pts), cos(pts), type = 'l', asp = 1) # test
+#
 # require(sp)
-# xy = cbind(x + r * sin(pts), y + r * cos(pts))
+# xy = cbind(x + 1 * sin(pts), y + 1 * cos(pts))
 # sl= SpatialPolygons(list(Polygons(list(Polygon(xy)), "Polygon")))
 # plot(sl, add=FALSE, col = 'red', axes=T )
-
-points
+#
+# points <- matrix(c(1, 5,
+#                    2, 0,
+#                    3, 4), ncol = 2, byrow = TRUE)
+#
+# plot(x = points[,1], y = points[, 2], asp = 1)
+#
+# points(sample_plots[, 1], sample_plots[, 2])
