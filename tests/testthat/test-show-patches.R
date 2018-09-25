d18ab525a32a08b66d4d66ae96b1ece9fc919277
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-09-25 12:47:14 +0200

    Some more tests for show_patches

--- tests/testthat/test-show-patches.R
+++ tests/testthat/test-show-patches.R
@@ -1,7 +1,25 @@
 context("show_patches")
 
-patches_plot <- show_patches(landscape)
+patches_landscape <- show_patches(landscape)
+
+patches_landscape_stack <- show_patches(landscape_stack, what = 1)
+
+patches_landscape_brick <- show_patches(landscape_brick, what = "all")
+
+patches_landscape_list <- show_patches(landscape_list, what = c(2,3))
+
 
 test_that("show_patches returns a plot", {
-    expect_equal(class(patches_plot), c("gg","ggplot"))
+    expect_equal(class(patches_landscape), c("gg","ggplot"))
+})
+
+test_that("show_patches returns a plot for each list entry", {
+    expect_true(all(sapply(patches_landscape_stack, FUN = function(x) class(x)[2]) == "ggplot"))
+    expect_true(all(sapply(patches_landscape_brick, FUN = function(x) class(x)[2]) == "ggplot"))
+    expect_true(all(sapply(patches_landscape_list, FUN = function(x) class(x)[2]) == "ggplot"))
+})
+
+test_that("show_patches returns error if class is not present", {
+    expect_error(show_patches(landscape, what = 42),
+                 regexp = "what must at least contain one value of a class contained in the landscape.")
 })
