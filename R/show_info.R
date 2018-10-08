5f6192afa3eb4ace7e50b955870307be8f3b2007
marco <sciaini.marco@gmail.com>
2018-10-08 11:12:32 +0200

    related to #38

--- R/show_info.R
+++ R/show_info.R
@@ -1,86 +1,69 @@
-#' Show info
+#' Check input landscape
 #'
-#' @description Show info
+#' @description Check input landscape
 #'
-#' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers
+#' @param landscape Raster* Layer, Stack, Brick, Stars or a list of rasterLayers
 #'
 #' @details This function extracts basic information about
 #' the input landscape.
 #' It includes a type of coordinate reference system (crs) -
 #' either "geographic", "projected", or NA,
-#' units of the coordinate reference system,
-#' and a class of the input landscape's values.
+#' units of the coordinate reference system, a class of the input landscape's
+#' values and the number of classes found in the landscape.
 #'
 #' @return tibble
 #'
 #' @examples
-#' show_info(augusta_nlcd)
-#' show_info(podlasie_ccilc)
-#' show_info(landscape)
+#' check_landscape(augusta_nlcd)
+#' check_landscape(podlasie_ccilc)
+#' check_landscape(landscape)
 #' landscape_stack = raster::stack(landscape, landscape)
-#' show_info(landscape_stack)
+#' check_landscape(landscape_stack)
 #'
-#' @aliases show_info
-#' @rdname show_info
+#' @aliases check_landscape
+#' @rdname check_landscape
 #'
 #' @export
-show_info <- function(landscape) UseMethod("show_info")
+check_landscape <- function(landscape) UseMethod("check_landscape")
 
-#' @name show_info
+#' @name check_landscape
 #' @export
-show_info.RasterLayer <- function(landscape) {
+check_landscape.RasterLayer <- function(landscape) {
 
-    result <- lapply(X = raster::as.list(landscape),
-           FUN = show_info_calc)
+   check_landscape_calc(raster::as.list(landscape))
 
-    dplyr::mutate(dplyr::bind_rows(result, .id = "layer"),
-                  layer = as.integer(layer))
 }
 
-#' @name show_info
+#' @name check_landscape
 #' @export
-show_info.RasterStack <- function(landscape) {
+check_landscape.RasterStack <- function(landscape) {
 
-    result <- lapply(X = raster::as.list(landscape),
-                     FUN = show_info_calc)
-
-    dplyr::mutate(dplyr::bind_rows(result, .id = "layer"),
-                  layer = as.integer(layer))
+    check_landscape_calc(raster::as.list(landscape))
 }
 
-#' @name show_info
+#' @name check_landscape
 #' @export
-show_info.RasterBrick <- function(landscape) {
+check_landscape.RasterBrick <- function(landscape) {
 
-    result <- lapply(X = raster::as.list(landscape),
-                     FUN = show_info_calc)
-
-    dplyr::mutate(dplyr::bind_rows(result, .id = "layer"),
-                  layer = as.integer(layer))
+    check_landscape_calc(raster::as.list(landscape))
 }
 
-#' @name show_info
+#' @name check_landscape
 #' @export
-show_info.stars <- function(landscape) {
+check_landscape.stars <- function(landscape) {
 
     landscape <- methods::as(landscape, "Raster")
 
-    result <- lapply(X = raster::as.list(landscape),
-                     FUN = show_info_calc)
+    check_landscape_calc(raster::as.list(landscape))
 
-    dplyr::mutate(dplyr::bind_rows(result, .id = "layer"),
-                  layer = as.integer(layer))
 }
 
-#' @name show_info
+#' @name check_landscape
 #' @export
-show_info.list <- function(landscape) {
+check_landscape.list <- function(landscape) {
 
-    result <- lapply(X = landscape,
-                     FUN = show_info_calc)
+    check_landscape_calc(landscape)
 
-    dplyr::mutate(dplyr::bind_rows(result, .id = "layer"),
-                  layer = as.integer(layer))
 }
 
 proj_info = function(landscape){
@@ -93,14 +76,70 @@ proj_info = function(landscape){
             data.frame(crs = "projected", units = proj_units)
         }
     } else {
-        data.frame(crs = NA, units = NA)
+        data.frame(crs = "NA", units = "NA")
     }
 }
 
 data_info <- function(landscape){
-    data.frame(class = class(raster::getValues(landscape)))
+    data.frame(class = class(raster::getValues(landscape)),
+               n_classes = length(raster::unique(landscape)))
 }
 
-show_info_calc <- function(landscape){
-    cbind(proj_info(landscape), data_info(landscape))
+check_landscape_calc <- function(landscape){
+
+    info <- lapply(X = landscape,
+           FUN = function(x){
+               x <- cbind(proj_info(x), data_info(x))
+               invisible(x)
+           })
+
+    info <- dplyr::mutate(dplyr::bind_rows(info, .id = "layer"),
+                  layer = as.integer(layer))
+
+    info <- dplyr::mutate(
+        info,
+        class_ok = dplyr::case_when(class != "integer" ~ "notok",
+                                    class == "integer" ~ "ok"),
+        units_ok = dplyr::case_when(units != "degrees" ~ "ok",
+                                    units == "degrees" ~ "notok"),
+        OK = dplyr::case_when(class_ok == "ok" & units_ok == "ok" ~ cli::symbol$tick,
+                              TRUE ~ cli::symbol$cross)
+    ) %>%
+        dplyr::select(layer, crs, units, class, n_classes, OK)
+
+    print(info)
+
+    correct_class <- info$class
+    correct_units <- info$units
+    correct_nclasses <- info$n_classes
+
+    if (any(correct_class != "integer")) {
+        cat("\n")
+        cat(crayon::yellow$bold("Caution:"))
+        cat("\n Only integer values for your classes are allowed as input for landscapemetrics.")
+
+    }
+
+    if (any(correct_units == "degrees")) {
+        cat("\n")
+        cat(crayon::yellow$bold("Caution:"))
+        cat(
+            "\n Only metric coordinates make sense for landscapemetrics. You will still get results, but these are not comparable with other studies and the interpretation of metrics that use the cellsize as input becomes error prone."
+        )
+
+    }
+
+    if (any(correct_nclasses > 30)) {
+        cat("\n")
+        cat(crayon::yellow$bold("Caution:"))
+        cat(
+            "\n Landscape metrics describe categorical landscape patterns.
+            You have more than 30 land cover classes, which seems very high (but can make sense in some cases, if so ignore this message).
+            However, if you did not think about classifying your landscapes before using landscapemetrics, we recommend to read our background vignette to familiarize yourself with
+            the basic concepts behind the metrics: https://r-spatialecology.github.io/landscapemetrics/articles/articles/general-background.html"
+        )
+
+    }
+
+    invisible(info)
 }
