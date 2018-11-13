4ad4158310e6b5f5f8a27892b3b391dd79646017
marco <sciaini.marco@gmail.com>
2018-11-13 15:30:36 +0100

    update tests

--- tests/testthat/test-check-landscape.R
+++ tests/testthat/test-check-landscape.R
@@ -2,21 +2,21 @@ context("check_landscape")
 
 test_that("calculate_lsm can take name argument", {
     check_tibble <- check_landscape(augusta_nlcd)
-    expect_true(check_tibble$OK == "✔")
+    expect_true(check_tibble$OK == cli::symbol$tick)
 })
 
 
 test_that("calculate_lsm can take name argument", {
     check_tibble <- check_landscape(podlasie_ccilc)
-    expect_true(check_tibble$OK == "✖")
+    expect_true(check_tibble$OK == cli::symbol$cross)
 })
 
 test_that("calculate_lsm can take name argument", {
     check_tibble <- check_landscape(landscape)
-    expect_true(check_tibble$OK == "?⃝")
+    expect_true(check_tibble$OK == cli::symbol$circle_question_mark)
 })
 
 test_that("calculate_lsm can take name argument", {
     check_tibble <- check_landscape(raster::stack(landscape, landscape))
-    expect_true(all(check_tibble$OK == "?⃝"))
+    expect_true(all(check_tibble$OK == cli::symbol$circle_question_mark))
 })
