66cd8d7a5192f023b37b5105d02ffd104d2180af
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-12-02 11:57:18 +0100

    get_boundaries (new version) can now handle rasters and matrix and checks for input

--- R/get_boundaries.R
+++ R/get_boundaries.R
@@ -1,62 +1,90 @@
 #' get_boundaries
 #'
-#' @description Streamlined version of raster::boundaries that takes a matrix as input
+#' @description Get boundary cells of patches
 #'
-#' @param matrix matrix with values.
-#' @param directions 4 (rooks case), 8 (queens case)
+#' @param landscape RasterLayer or matrix.
+#' @param rooks_case Logical if rook's case (4 neighbours) or queen's
+#' case (8 neighbours) should be used as neighbourhood rule
+#' @param return_raster If false, matrix is returned
 #'
 #' @details
-#' Stripped down version of raster::boundaries so that we internally can parse
-#' a matrix.
+#' All boundary/edge cells are labeled 1, all non-boundary cells 0. NA values are
+#' not changed. Boundary cells are defined as cells that neighbour either a NA
+#' cell or a cell with a different value than itself. Non-boundary cells only
+#' neighbour cells with the same value than themself.
 #'
-#' @return matrix
+#' @return RasterLayer or matrix
 #'
 #' @examples
-#' test_matrix <- raster::as.matrix(landscape)
-#' get_boundaries(test_matrix)
+#' class_1 <- get_patches(landscape, class = 1)[[1]]
 #'
-#' @aliases matrix_to_raster
-#' @rdname matrix_to_raster
+#' get_boundaries(class_1)
+#' get_boundaries(class_1, return_raster = FALSE)
 #'
-#' @author Robert Hijmans
-#' @keywords internal
+#' class_1_matrix <- raster::as.matrix(class_1)
+#' get_boundaries(class_1_matrix, return_raster = FALSE)
+#'
+#' @aliases get_boundaries
+#' @rdname get_boundaries
 #'
 #' @export
+get_boundaries <- function(landscape, rooks_case = TRUE, return_raster = TRUE){
 
-get_boundaries <- function(landscape, directions = 4){
+    # check if either raster or matrix is provided
+    if(class(landscape) != "RasterLayer" && class(landscape) != "matrix") {
+        stop("Please provide RasterLayer or matrix as input.", call. = FALSE)
+    }
 
+    # input is raster
     if(class(landscape) == "RasterLayer") {
-        landscape <- raster::as.matrix(landscape)
+
+        # get boundaries
+        result <- rcpp_get_boundaries(raster::as.matrix(landscape),
+                                      rooks_case = rooks_case)
+
+        # convert back to raster
+        if(return_raster) {
+            result <- matrix_to_raster(matrix = result,
+                                       landscape = landscape)
+        }
+
+        return(result)
     }
 
-    if(class(landscape) != "matrix") {
-        stop("Please provide matrix as input.", call. = FALSE)
+    # input is matrix
+    else{
+
+        # get boundaries
+        result <- rcpp_get_boundaries(landscape,
+                                      rooks_case = rooks_case)
+
+        if(return_raster) {
+            warning("return_raster = TRUE not able for matrix input")
+        }
+
+        return(result)
     }
-
-    landscape <- cbind(landscape[, 1], landscape, landscape[, ncol(landscape)])
-    landscape <- rbind(landscape[1,], landscape, landscape[nrow(landscape),])
-    paddim <- as.integer(dim(landscape))
-    classes <- 0L
-    type <- 0L
-    directions <- as.integer(directions)
-
-    x <- .Call('_edge', as.integer(t(landscape)), paddim, classes, type, directions, NAOK=TRUE, PACKAGE='raster')
-    x <- matrix(x, nrow=paddim[1], ncol=paddim[2], byrow=TRUE)
-    x <- x[2:(nrow(x)-1), 2:(ncol(x)-1)]
-    return(x)
 }
 
-get_boundaries2 <- function(landscape, rooks_case = TRUE){
-
-    if(class(landscape) == "RasterLayer") {
-        landscape <- raster::as.matrix(landscape)
-    }
-
-    if(class(landscape) != "matrix") {
-        stop("Please provide matrix as input.", call. = FALSE)
-    }
-
-    boundaries <- rcpp_get_boundaries(landscape, rooks_case)
-
-    return(boundaries)
-}
+# get_boundaries_old <- function(landscape, directions = 4){
+#
+#     if(class(landscape) == "RasterLayer") {
+#         landscape <- raster::as.matrix(landscape)
+#     }
+#
+#     if(class(landscape) != "matrix") {
+#         stop("Please provide matrix as input.", call. = FALSE)
+#     }
+#
+#     landscape <- cbind(landscape[, 1], landscape, landscape[, ncol(landscape)])
+#     landscape <- rbind(landscape[1,], landscape, landscape[nrow(landscape),])
+#     paddim <- as.integer(dim(landscape))
+#     classes <- 0L
+#     type <- 0L
+#     directions <- as.integer(directions)
+#
+#     x <- .Call('_edge', as.integer(t(landscape)), paddim, classes, type, directions, NAOK=TRUE, PACKAGE='raster')
+#     x <- matrix(x, nrow=paddim[1], ncol=paddim[2], byrow=TRUE)
+#     x <- x[2:(nrow(x)-1), 2:(ncol(x)-1)]
+#     return(x)
+# }
