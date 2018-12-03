750d0eadccfd19aa0fd0ea498c512faca21a3454
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-12-03 14:52:31 +0100

    raster_to_points can also not return NA

--- R/raster_to_points.R
+++ R/raster_to_points.R
@@ -3,6 +3,7 @@
 #' @description Raster to points
 #'
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
+#' @param return_NA If true, NA cells are also included
 #'
 #' @details
 #' Wrapper around raster::xyFromCell and raster::getValues to get raster_to_points
@@ -19,13 +20,20 @@
 #' @keywords internal
 #'
 #' @export
-raster_to_points <- function(landscape){
+raster_to_points <- function(landscape, return_NA = TRUE){
 
     # get coordinates
     xyz <- raster::xyFromCell(landscape, cell = 1:raster::ncell(landscape))
 
     # add values including NA
     xyz <- cbind(xyz, raster::getValues(landscape))
+
+
+    if(!return_NA) {
+        xyz <- xyz[!is.na(xyz[, 3]), ]
+    }
+
+    colnames(xyz) <- c("x", "y", "z")
 
     return(xyz)
 }
