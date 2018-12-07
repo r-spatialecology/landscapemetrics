5946bb3556a94c7fc0cd248bf211e0ebaf64d7ac
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2018-12-07 16:08:35 +0100

    Remove example get_adj

--- R/get_adjacencies.R
+++ R/get_adjacencies.R
@@ -5,7 +5,7 @@
 #' @param landscape Raster* Layer, Stack, Brick or a list of rasterLayers.
 #' @param neighbourhood The number of directions in which cell adjacencies are
 #' considered as neighbours: 4 (rook's case), 8 (queen's case) or a binary matrix
-#'  where the ones define the neighbourhood. The default is 4.
+#' where 1 define the neighbourhood and 0 the focal cell. The default is 4.
 #' @param what Which adjacencies to calculate: "full" for a full adjacency matrix,
 #' "like" for the diagonal, "unlike" for the off diagonal part of the matrix and
 #'  "triangle" for a triangular matrix counting adjacencies only once.
@@ -27,13 +27,7 @@
 #'
 #' @examples
 #' # calculate full adjacency matrix
-#' get_adjacencies(landscape, 4)
-#'
-#' # count diagonal neighbour adjacencies
-#' diagonal_matrix <- matrix(c(1,  NA,  1,
-#'                             NA,  0, NA,
-#'                             1,  NA,  1), 3, 3, byrow = TRUE)
-#' get_adjacencies(landscape, diagonal_matrix)
+#' get_adjacencies(landscape, neighbourhood = 4)
 #'
 #' @aliases get_adjacencies
 #' @rdname get_adjacencies
