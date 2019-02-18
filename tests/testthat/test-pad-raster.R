6e0d4f0a444000ea006474c47603d323a58d8408
Maximilian Hesselbarth <maximilian.hesselbarth@uni-goettingen.de>
2019-02-18 08:14:42 +0100

    Update test pad_raster

--- tests/testthat/test-pad-raster.R
+++ tests/testthat/test-pad-raster.R
@@ -1,15 +1,29 @@
-# context("pad_raster")
-#
-# test_that("pad_raster can handle all raster inputs", {
-#     expect_is(landscapemetrics:::pad_raster(landscape), "RasterLayer")
-#     expect_is(landscapemetrics:::pad_raster(landscape_stack), "list")
-#     expect_is(landscapemetrics:::pad_raster(landscape_brick), "list")
-#     expect_is(landscapemetrics:::pad_raster(landscape_list), "list")
-# })
-#
-# test_that("pad_raster can handle options", {
-#     expect_is(landscapemetrics:::pad_raster(landscape,
-#                          pad_raster_value = -5,
-#                          pad_raster_cells = 5,
-#                          global = TRUE), "RasterLayer")
-# })
+context("pad_raster")
+
+test_that("pad_raster can handle all raster inputs", {
+    expect_is(landscapemetrics:::pad_raster(landscape), "list")
+    expect_is(landscapemetrics:::pad_raster(landscape_stack), "list")
+    expect_is(landscapemetrics:::pad_raster(landscape_brick), "list")
+    expect_is(landscapemetrics:::pad_raster(landscape_list), "list")
+    expect_is(landscapemetrics:::pad_raster(raster::as.matrix(landscape)), "list")
+})
+
+test_that("pad_raster can add more than one row", {
+
+    result <- landscapemetrics:::pad_raster(landscape,
+                                            pad_raster_cells = 5,
+                                            global = TRUE)[[1]]
+
+    # original landscape is 30 x 30 and adding 5 rows/cols in each direction
+    expect_equal(object = nrow(result), expected = 40)
+    expect_equal(object = ncol(result), expected = 40)
+})
+
+test_that("pad_raster can add different padding values", {
+
+    result <- landscapemetrics:::pad_raster(landscape,
+                                            pad_raster_value = NA,
+                                            global = TRUE)[[1]]
+
+    expect_true(anyNA(result))
+})
