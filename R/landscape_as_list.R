7bdd7021a812cd9454f044590ddc6559419c1ed0
Nowosad <tupiszakaczki@gmail.com>
2020-08-19 15:04:24 +0200

    adds landscape_as_list

--- R/landscape_as_list.R
+++ R/landscape_as_list.R
@@ -1,22 +1,22 @@
-prepare_input <- function(landscape) UseMethod("prepare_input")
+landscape_as_list <- function(landscape) UseMethod("prepare_input")
 
-prepare_input.RasterLayer <- function(landscape){
+landscape_as_list.RasterLayer <- function(landscape){
     landscape <- raster::as.list(landscape)
     return(landscape)
 }
-prepare_input.RasterStack <- function(landscape){
+lsm_as_list.RasterStack <- function(landscape){
     landscape <- raster::as.list(landscape)
     return(landscape)
 }
-prepare_input.RasterBrick <- function(landscape){
+landscape_as_list.RasterBrick <- function(landscape){
     landscape <- raster::as.list(landscape)
     return(landscape)
 }
-prepare_input.stars <- function(landscape){
+landscape_as_list.stars <- function(landscape){
     landscape <- methods::as(landscape, "Raster")
     landscape <- raster::as.list(landscape)
     return(landscape)
 }
-prepare_input.list <- function(landscape){
+landscape_as_list.list <- function(landscape){
     return(landscape)
 }
