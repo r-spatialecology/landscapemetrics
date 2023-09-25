df3144b51f96f81e30f5756a98d24fe3f744a5b5
mhesselbarth <mhk.hesselbarth@gmail.com>
2023-09-25 10:28:38 +0200

    Tiny update to startup (gh only)

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,8 @@
 .onAttach <- function(libname, pkgname){
 
-    packageStartupMessage("Starting from v2.0.0, landscapemetrics does not support the 'raster' or 'sp' packages.
-    They are replaced by 'terra' and 'sf', respectively. More information
-    about the 'terra' package can be found here: https://rspatial.org/index.html.")
-
+    packageStartupMessage(
+    "Starting from v2.0, landscapemetrics uses the 'terra' and 'sf' instead of 'raster' and 'sp'.
+    This might results in a slightly different behavior of the code.
+    More information about the 'terra' package can be found here: https://rspatial.org/index.html."
+    )
 }
