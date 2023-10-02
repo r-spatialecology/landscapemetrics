7cc986297f8010dfa625052d135c7a5e38cd1554
mhesselbarth <mhk.hesselbarth@gmail.com>
2023-10-02 09:37:13 +0200

    Remove startup message

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.onAttach <- function(libname, pkgname){
-
-    packageStartupMessage(
-    "Starting from v2.0, landscapemetrics uses the 'terra' and 'sf' instead of 'raster' and 'sp'.
-    This might results in a slightly different behavior of the code.
-    More information about the 'terra' package can be found here: https://rspatial.org/index.html."
-    )
-}
