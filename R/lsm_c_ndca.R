b84c6c7c227eec59e04ef816a00684347c9c2dc7
Jakub Nowosad <tupiszakaczki@gmail.com>
2023-10-05 21:05:34 +0200

    fixes missing resolution

--- R/lsm_c_ndca.R
+++ R/lsm_c_ndca.R
@@ -65,13 +65,14 @@ lsm_c_ndca <- function(landscape, directions = 8, consider_boundary = FALSE, edg
     tibble::add_column(result, layer, .before = TRUE)
 }
 
-lsm_c_ndca_calc <- function(landscape, directions, consider_boundary, edge_depth, extras = NULL){
+lsm_c_ndca_calc <- function(landscape, directions, consider_boundary, edge_depth, resolution, extras = NULL){
 
     # get number of core areas for each patch
     ndca <- lsm_p_ncore_calc(landscape,
                              directions = directions,
                              consider_boundary = consider_boundary,
                              edge_depth = edge_depth,
+                             resolution = resolution,
                              extras = extras)
 
     # all cells are NA
