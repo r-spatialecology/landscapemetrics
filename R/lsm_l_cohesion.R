611f0436ba4f5a80bdb153f393def303581ac187
toddellis <toddellis.wa@gmail.com>
2025-10-10 09:42:44 +1100

    Reverted formatting for lsm_l_cohesion

--- R/lsm_l_cohesion.R
+++ R/lsm_l_cohesion.R
@@ -42,88 +42,65 @@
 lsm_l_cohesion <- function(landscape, directions = 8) {
     landscape <- landscape_as_list(landscape)
 
-    result <- lapply(
-        X = landscape,
-        FUN = lsm_l_cohesion_calc,
-        directions = directions
-    )
+    result <- lapply(X = landscape,
+                     FUN = lsm_l_cohesion_calc,
+                     directions = directions)
 
-    layer <- rep(
-        seq_along(result),
-        vapply(result, nrow, FUN.VALUE = integer(1))
-    )
+    layer <- rep(seq_along(result),
+                 vapply(result, nrow, FUN.VALUE = integer(1)))
 
     result <- do.call(rbind, result)
 
     tibble::add_column(result, layer, .before = TRUE)
 }
 
-lsm_l_cohesion_calc <- function(
-    landscape,
-    directions,
-    resolution,
-    extras = NULL
-) {
-    if (missing(resolution)) {
-        resolution <- terra::res(landscape)
-    }
+lsm_l_cohesion_calc <- function(landscape, directions, resolution, extras = NULL) {
 
-    if (is.null(extras)) {
+    if (missing(resolution)) resolution <- terra::res(landscape)
+
+    if (is.null(extras)){
         metrics <- "lsm_l_cohesion"
         landscape <- terra::as.matrix(landscape, wide = TRUE)
-        extras <- prepare_extras(
-            metrics,
-            landscape_mat = landscape,
-            directions = directions,
-            resolution = resolution
-        )
+        extras <- prepare_extras(metrics, landscape_mat = landscape,
+                                            directions = directions, resolution = resolution)
     }
 
     # all values NA
     if (all(is.na(landscape))) {
-        return(tibble::new_tibble(list(
-            level = "landscape",
-            class = as.integer(NA),
-            id = as.integer(NA),
-            metric = "cohesion",
-            value = as.double(NA)
-        )))
+        return(tibble::new_tibble(list(level = "landscape",
+                              class = as.integer(NA),
+                              id = as.integer(NA),
+                              metric = "cohesion",
+                              value = as.double(NA))))
     }
 
     # get number of cells
     ncells_landscape <- length(landscape[!is.na(landscape)])
 
     # get number of cells in each patch: area = n_cells * res / 10000
-    ncells_patch <- lsm_p_area_calc(
-        landscape,
-        directions = directions,
-        resolution = resolution,
-        extras = extras
-    )
+    ncells_patch <- lsm_p_area_calc(landscape,
+                                    directions = directions,
+                                    resolution = resolution,
+                                    extras = extras)
 
     ncells_patch$value <- ncells_patch$value * 10000 / prod(resolution)
 
     # get perim for each patch
-    perim_patch <- lsm_p_perim_calc(
-        landscape,
-        directions = directions,
-        resolution = resolution,
-        extras = extras
-    )
+    perim_patch <- lsm_p_perim_calc(landscape,
+                                    directions = directions,
+                                    resolution = resolution,
+                                    extras = extras)
 
     # denominator for cohesion (perim / n_cells) for landscape
     denominator <- sum(perim_patch$value * sqrt(ncells_patch$value))
 
     # calcualte cohesion
     cohesion <- (1 - (sum(perim_patch$value) / denominator)) *
-        ((1 - (1 / sqrt(ncells_landscape)))^-1) *
-        100
+        ((1 - (1 / sqrt(ncells_landscape))) ^ -1) * 100
 
-    return(tibble::new_tibble(list(
-        level = rep("landscape", length(cohesion)),
-        class = rep(as.integer(NA), length(cohesion)),
-        id = rep(as.integer(NA), length(cohesion)),
-        metric = rep("cohesion", length(cohesion)),
-        value = as.double(cohesion)
-    )))
+    return(tibble::new_tibble(list(level = rep("landscape", length(cohesion)),
+                 class = rep(as.integer(NA), length(cohesion)),
+                 id = rep(as.integer(NA), length(cohesion)),
+                 metric = rep("cohesion", length(cohesion)),
+                 value = as.double(cohesion))))
 }
